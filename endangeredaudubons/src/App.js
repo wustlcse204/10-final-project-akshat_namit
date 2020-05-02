6cdb2d1ed767e496f07f73aae94857615e1a9a7d
akshatksharma <akshatksharma@wustl.edu>
2020-05-02 14:07:33 -0500

    update

--- endangeredaudubons/src/App.js
+++ endangeredaudubons/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 import {database} from './database.js'
 import TopBar from './topbar/TopBar'
 import About from './about/About'
